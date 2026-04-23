6839241e4bb4bb63978f2510fd00bddd891069b1
Leonardo Costa Gomes <leonardocosta0910@gmail.com>
2026-04-23 11:28:42 -0300

    Criando a classe Image

---
+++ Power Lab/src/Models/Image.php
@@ -0,0 +1,32 @@
+<?php
+    class Image
+    {
+        public function __construct(
+            private string $name = "none",
+            private string $data,
+            private string $mimeType
+            ) { }
+
+
+        public function getBase64Src() : string
+        {
+            $base64 = base64_encode($this->data);
+            return "data:{$this->mimeType};base64,{$base64}";
+        }
+
+        public function getRawData() : string
+        {
+            return $this->data;
+        }
+
+        public function getMimeType() : string 
+        {
+            return $this->mimeType;
+        }
+
+        public function getName() : string
+        {
+            return $this->name;
+        }
+    }
+?>
