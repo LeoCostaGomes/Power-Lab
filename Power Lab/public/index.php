f733f2cf9fcb8d5b414f83960a3bd817afd776b0
Leonardo Costa Gomes <leonardocosta0910@gmail.com>
2026-04-29 09:47:05 -0300

    Update index.php

--- Power Lab/public/index.php
+++ Power Lab/public/index.php
@@ -1,18 +1,12 @@
 <?php
-    spl_autoload_register(function ($nomeDaClasse) {
-    // Procura o arquivo com o nome da classe na mesma pasta
-    $caminho = __DIR__ . '/' . $nomeDaClasse . '.php';
-    
-    if (file_exists($caminho)) {
-        require_once $caminho;
+    require __DIR__ . '/../autoload.php';
+
+    use App\Models\IP;
+
+    try {
+        $meuIp = new IP("192.168.0.1");
+        echo "IP válido: " . $meuIp->getIP();
+    } catch (\Exception $e) {
+        echo "Erro: " . $e->getMessage();
     }
-});
-
-
-try {
-    $meuIp = new IP("192.168.0.1");
-    echo "IP válido: " . $meuIp->getIP();
-} catch (\Exception $e) {
-    echo "Erro: " . $e->getMessage();
-}
 ?>
