ddbf371f244c1791e082c4d89a771f2dc94e6fb8
Leonardo Costa Gomes <leonardocosta0910@gmail.com>
2026-05-07 09:49:29 -0300

    Refactor User class to include password handling

--- Power Lab/src/Models/User.php
+++ Power Lab/src/Models/User.php
@@ -1,32 +1,51 @@
 <?php
-    namespace App\Models;
+namespace App\Models;
 
-    use App\Models\IP;
-    use App\Models\Email;
-    class User
-    {
-        public function __construct(
-            private string $name,
-            private string $password,
-            private Email $email,
-            private IP $ip,
-            ) {
-        }
-
-        public function getName(): string
-        {
-            return $this->name;
-        }
-
-        public function setName(string $name)
-        {
-            if ($name === "")  return;
-            $this->name = $name;
-        }
-
-        public function comparePassword(string $passwordToCompare): bool
-        {
-            return password_verify($passwordToCompare, $this->password);
-        }
+use App\Models\IP;
+use App\Models\Email;
+class User
+{
+    public function __construct(
+        private string $name,
+        private string $password,
+        private Email $email,
+        private IP $ip,
+    ) {
+        $this->password = $this->setPassword($password);
     }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        if ($name === "")
+            throw new \InvalidArgumentException("Invalid name.");
+        $this->name = $name;
+    }
+
+    public function comparePassword(string $passwordToCompare): bool
+    {
+        $passwordToCompare = password_hash($passwordToCompare, PASSWORD_DEFAULT);
+        return password_verify($passwordToCompare, $this->password);
+    }
+
+    public function alterPassword(string $OldPassword, string $NewPassword)
+    {
+        if (!$this->comparePassword($OldPassword))
+            throw new \InvalidArgumentException("old password is incorrect.");
+        $this->password = $this->setPassword($NewPassword);
+    }
+
+    private function setPassword(string $password) : string
+    {
+        if ($password === "")
+            throw new \InvalidArgumentException("Invalid password.");
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    
+}
 ?>
