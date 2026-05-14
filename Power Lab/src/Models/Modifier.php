5f8bf80b78933c49c43c996b6c13f3bcc1f39742
Leonardo Costa Gomes <leonardocosta0910@gmail.com>
2026-05-14 08:36:35 -0300

    Add files via upload

---
+++ Power Lab/src/Models/Modifier.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+
+use App\Models\Image;
+
+class Modifier 
+{
+        private string $name;
+        private string $description;
+        private Image $spriteIcon;
+
+        public function __construct(
+            string $name,
+            string $description,
+            Image $spriteIcon
+            ) {
+                $this->
+
+            } 
+
+
+
+}
