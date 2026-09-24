6836a7c483257c20eea123e7829bd6f9d3a4f77d
caualcosta303 <caualcosta0408@gmail.com>
2026-09-24 09:25:44 -0300

    Arrumando problema do cors

--- Power Lab/public/index.php
+++ Power Lab/public/index.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, Accept");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
 
 require_once __DIR__ . '/../autoloader.php';
 require_once __DIR__ . '/../bootstrap.php';
