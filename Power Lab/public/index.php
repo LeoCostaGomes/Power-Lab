c2485c8e4b121bea0311a18d65201467ce7e1cc5
Ratorto <leonardocosta0910@gmail.com>
2026-09-25 21:27:37 -0300

    Arrumando problema do cors com as telas de cadastro e login

--- Power Lab/public/index.php
+++ Power Lab/public/index.php
@@ -3,6 +3,10 @@ header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, Accept");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit(0);
+}
 
 require_once __DIR__ . '/../autoloader.php';
 require_once __DIR__ . '/../bootstrap.php';
